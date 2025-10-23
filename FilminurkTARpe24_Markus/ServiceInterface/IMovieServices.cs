54231969d4388a8bcd26a834af057d0fb4efffe7
markussamberk <markussamberk@gmail.com>
2025-10-23 11:41:28 +0300

    modified createupdate view
    .

--- FilminurkTARpe24_Markus/ServiceInterface/IMovieServices.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FilminurkTARpe24_Markus.ServiceInterface
-{
-    public interface IMovieServices //see on interface. asub .core/serviceinterface
-    {
-        Task<Movie> Create(MoviesDTO dto);
-        Task<Movie> DetailsAsync(Guid id);
-    }
-}
