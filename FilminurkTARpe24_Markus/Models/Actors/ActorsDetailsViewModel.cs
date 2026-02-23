e6ee456bf97e54d334534fd091cf02e1bbca184d
markussamberk <markussamberk@gmail.com>
2026-02-23 08:58:56 +0200

    Added Actors ViewModels

---
+++ FilminurkTARpe24_Markus/Models/Actors/ActorsDetailsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Filminurk.Models.Actors;
+
+namespace FilminurkTARpe24_Markus.Models.Actors
+{
+    public class ActorsDetailsViewModel
+    {
+        public Guid ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public List<string>? MoviesActedFor { get; set; }
+        public Guid PortraitID { get; set; }
+
+        // 3 õpilase andmetüüpi
+        public ActorRating? ActorRating { get; set; }
+        public int? Salary { get; set; }
+        public int? Height { get; set; }
+
+        // andmebaasi jaoks
+        public DateTime? EntryCreatedAt { get; set; }
+        public DateTime? EntryModifiedAt { get; set; }
+    }
+}
---
+++ FilminurkTARpe24_Markus/Models/Actors/ActorsIndexViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Filminurk.Core.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace Filminurk.Models.Actors
+{
+    public class ActorsIndexViewModel
+    {
+        public Guid ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public List<string>? MoviesActedFor { get; set; }
+        public Guid PortraitID { get; set; }
+
+        // 3 õpilase andmetüüpi
+        public ActorRating? ActorRating { get; set; }
+        public int? Salary { get; set; }
+        public int? Height { get; set; }
+    }
+    public enum ActorRating
+    {
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5
+    }
+}
